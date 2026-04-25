547d7c8116589cbb5ac3c8fd7673e1dd325f3e6c
Mario Rojas <rojasespinolamario@gmail.com>
2026-04-25 18:52:18 -0500

    Termine de implementar la clase Cuaqueo y Chirreo

--- src/semana3/Chirreo.java
+++ src/semana3/Chirreo.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package semana3;
 
-/**
- *
- * @author sistemas
- */
 public class Chirreo {
-    
+
+    public void cuaquear () {
+        System.out.println("Chirr Chirr");
+    }
 }
--- src/semana3/Cuaqueo.java
+++ src/semana3/Cuaqueo.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package semana3;
 
-/**
- *
- * @author sistemas
- */
 public class Cuaqueo {
-    
+
+    public void cuaquear () {
+        System.out.println("Soy un verdadero pato por eso hago CUAC CUAC");
+    }
 }
